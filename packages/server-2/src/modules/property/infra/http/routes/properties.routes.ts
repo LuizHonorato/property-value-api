9f0bc096cee4e077590f5c4c0111d6ed27a1355b
LuizHonorato <lhehonorato@gmail.com>
2021-02-28 15:19:16 -0300

    implementing api request.

---
+++ packages/server-2/src/modules/property/infra/http/routes/properties.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+import PropertyValueController from '../controllers/PropertyValueController';
+
+const propertiesRouter = Router();
+const propertyValueController = new PropertyValueController();
+
+propertiesRouter.get('/get-property-value', celebrate({
+  [Segments.BODY]: {
+    meters: Joi.number().required(),
+  },
+}), propertyValueController.index);
+
+export default propertiesRouter;
--- packages/server-2/src/shared/infra/http/server.ts
+++ packages/server-2/src/shared/infra/http/server.ts
@@ -1,12 +1,32 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-app.get('/test2', (req, res) => res.send({ ok: true }));
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: err.stack,
+  });
+});
 
 app.listen(3002, () => {
   console.log('Server 2 started on port 3002! 🚀️');
